7e7c0c330a543516fd3bcae85e88c177dd3b2a05
Rahul Senna <rahulvsenna@gmail.com>
2026-05-15 10:49:44 +0530

    installing nasm on runner

--- .codecrafters/compile.sh
+++ .codecrafters/compile.sh
@@ -7,6 +7,20 @@
 # Learn more: https://codecrafters.io/program-interface
 
 set -e # Exit on failure
+uname -a
+OLDPWD="$(pwd)"
+echo "$OLDPWD"
+
+curl -L -o /tmp/nasm-3.01.tar.xz https://www.nasm.us/pub/nasm/releasebuilds/3.01/nasm-3.01.tar.xz && \
+mkdir -p /tmp/nasm-build && cd /tmp/nasm-build && \
+tar -xf /tmp/nasm-3.01.tar.xz && cd nasm-3.01 && \
+./configure --prefix="$HOME/.local" && make -j"$(getconf _NPROCESSORS_ONLN 2>/dev/null || echo 2)" && make install && \
+export PATH="$HOME/.local/bin:$PATH" && nasm --version
+
+echo "$OLDPWD"
+
+cd "$OLDPWD"
+cd /app
 
 cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
 cmake --build ./build
